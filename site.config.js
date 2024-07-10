12e0a72bf7d9f84d7e53dd0eeaaccdd1b8be8f9b
JIANGBO.WANG <50077834+JiangboWANGfr@users.noreply.github.com>
2024-07-10 16:06:50 +0200

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -26,7 +26,7 @@ const CONFIG = {
   // CONFIG configration (required)
   link: "https://raphaelblog.vercel.app",
   since: 2024, // If leave this empty, current year will be used.
-  lang: "zh-CN", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
